1849c21e64f491be71098fb669d7f438275cba77
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-02-27 15:34:57 +1100

    working game with x/o image, hover background, new game button

--- js/main.js
+++ js/main.js
@@ -1,75 +1,106 @@
-// Things needed to be fixed/do
-// - Shifting of box when data get entered
-// - Check why winner message got printed before box .html update
+// To check
+// - Why doesn't .box:hover working
+// - How to "remove/hide" background image of .box ('x' and 'o')
+
 
 
 $(document).ready(function() {
+  $winnerMessage = $('#winner-message');
+  $currentScore = $('#current-score');
+
+  var scoreBoard = {
+    player1: 0,
+    player2: 0
+  }
+
+  var updateScoreboard = function() {
+    $currentScore.html(scoreBoard.player1 + ' - ' + scoreBoard.player2);
+  }
+
+  var initializeGame = function() {
+    tictactoe.board = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]];
+    tictactoe.winner = 0;
+    tictactoe.currentPlayer = 1;
+    $(".box").removeClass("x");
+    $(".box").removeClass("o");
+    $(".box").addClass("clear");
+    $winnerMessage.hide();
+  }
+
   var tictactoe = {
+
     turnCounter: 0,
     currentPlayer: 1,
     winner: 0,
     board: [[0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]],
+
     getPosition: function(boxId) {
       var row = boxId[0];
       var col = boxId[2];
       this.pushInput(row, col);
     },
+
     pushInput: function(row, col) {
       if(this.currentPlayer === 1) {
-        this.board[row][col] = 1;
-        $('#' + row + '-' + col).html(this.currentPlayer);
-        this.currentPlayer = 2;
+        if (this.board[row][col] === 0) {
+          this.board[row][col] = 1;
+          $('#' + row + '-' + col).removeClass("clear");
+          $('#' + row + '-' + col).addClass("x");
+          this.currentPlayer = 2;
+        }
+
       } else {
-        this.board[row][col] = 2;
-        $('#' + row + '-' + col).html(this.currentPlayer);
-        this.currentPlayer = 1;
+        if (this.board[row][col] === 0) {
+          this.board[row][col] = 2;
+          $('#' + row + '-' + col).removeClass("clear");
+          $('#' + row + '-' + col).addClass("o");
+          this.currentPlayer = 1;
+        }
       }
 
-      console.log(this.board);
-      this.checkRow(row);
-      this.checkCol(col);
-      if ((row+col)%2 === 0){
-        this.checkDiag();
-      }
-
+      this.checkWinner(row, col);
+      console.log("board: " + this.board);
+      console.log("winner: " + this.winner);
     },
-    checkRow: function(row) {
+    checkWinner: function(row, col) {
+      // Checking process
       if ((this.board[row][0] === this.board[row][1]) && (this.board[row][1] === this.board[row][2])) {
         this.winner = this.board[row][0];
+      } else if ((this.board[0][col] === this.board[1][col]) && (this.board[1][col] === this.board[2][col])) {
+          this.winner = this.board[0][col];
+      } else if((row+col)%2 === 0) {
+        if (((this.board[0][0] === this.board[1][1]) && (this.board[1][1] === this.board[2][2])) ||
+            ((this.board[2][0] === this.board[1][1]) && (this.board[1][1] === this.board[0][2]))) {
+          this.winner = this.board[1][1];
+        }
       }
-
+      // Winner message
       if (this.winner !== 0) {
-        alert('The winner is player ' + this.winner);
+        scoreBoard['player'+this.winner]++;
+        updateScoreboard();
+        $winnerMessage = $('#winner-message');
+        $winnerMessage.show();
+        $winnerMessage.html('The winner is player ' + this.winner + '!');
+
       }
     },
-    checkCol: function(col) {
-      if ((this.board[0][col] === this.board[1][col]) && (this.board[1][col] === this.board[2][col])) {
-        this.winner = this.board[0][col];
-      }
-
-      if (this.winner !== 0) {
-        alert('The winner is player ' + this.winner);
-      }
-    },
-    checkDiag: function() {
-      if (((this.board[0][0] === this.board[1][1]) && (this.board[1][1] === this.board[2][2])) ||
-          ((this.board[2][0] === this.board[1][1]) && (this.board[1][1] === this.board[0][2]))) {
-        this.winner = this.board[1][1];
-      }
-
-      if (this.winner !== 0) {
-        alert('The winner is player ' + this.winner);
-      }
-    }
   };
 
-
+  // $winnerMessage.hide();
+  initializeGame();
+  updateScoreboard();
 
   $(document).on('click', '.box', function() {
     var clickedId = $(this).attr('id');
     tictactoe.getPosition(clickedId);
+  });
+
+  $(document).on('click', '#new-game', function() {
+    initializeGame();
   });
 
 
