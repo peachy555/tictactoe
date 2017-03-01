fe1fa2a3bff6279ebefc804d6d355e8cdf0d8205
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-03-01 11:48:47 +1100

    localStorage completed

--- js/main.js
+++ js/main.js
@@ -13,41 +13,78 @@
 // window.firebaseExample = new Firebase('https://docs-examples.firebaseio.com/web/data');
 
 $(document).ready(function() {
+
+
   $winnerMessage = $('#winner-message');
   $currentScore = $('.current-score');
 
   var boardSize = 'normal';
-  var scoreBoard = {
-    player1: 0,
-    player2: 0
+
+  var initializeGame = function() {
+    var localGame = localStorage.getItem("tictactoeState");
+    if(localGame){
+      tictactoe.state = JSON.parse(localGame);
+    }
+    for (var row = 0; row < 3; row++) {
+      for (var col = 0; col< 3; col++) {
+        if (tictactoe.state.board[row][col] === 1) {
+          $('#' + row + '-' + col).removeClass("clear");
+          $('#' + row + '-' + col).addClass("o");
+        } else if (tictactoe.state.board[row][col] === 2) {
+          $('#' + row + '-' + col).removeClass("clear");
+          $('#' + row + '-' + col).addClass("x");
+        }
+      }
+    }
+    updateScoreboard();
   }
 
   var updateScoreboard = function() {
-    $currentScore.html(scoreBoard.player1 + ' - ' + scoreBoard.player2);
+    $currentScore.html(tictactoe.state.scoreBoard.player1.score + ' - ' + tictactoe.state.scoreBoard.player2.score);
   }
 
-  var initializeGame = function() {
-    tictactoe.board = [[0, 0, 0],
-                      [0, 0, 0],
-                      [0, 0, 0]];
-    tictactoe.winner = 0;
-    tictactoe.currentPlayer = 1;
-    tictactoe.turnCounter = 0;
-    $(".box").removeClass("x");
-    $(".box").removeClass("o");
-    $(".box").addClass("clear");
-  }
+  var resetGame = function() {
+    $("#board").fadeOut(800, function() {
+      tictactoe.state = {
+        turnCounter: 0,
+        currentPlayer: 1,
+        winner: 0,
+        board: [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]],
+        scoreBoard: tictactoe.state.scoreBoard
+              };
+        $(".box").removeClass("x");
+        $(".box").removeClass("o");
+        $(".box").addClass("clear");
+        updateScoreboard();
+        localStorage.setItem("tictactoeState", JSON.stringify(tictactoe.state));
+    });
+    $("#board").fadeIn(800);
 
-  var tictactoe = {
-    isSinglePlay: true,
-    turnCounter: 0,
-    currentPlayer: 1,
-    winner: 0,
-    board: [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]],
-    markingScheme: {
+  };
 
+
+  window.tictactoe = {
+
+    state: {
+      isSinglePlay: false,
+      turnCounter: 0,
+      currentPlayer: 1,
+      winner: 0,
+      board: [[0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0]],
+      scoreBoard: {
+        player1: {
+          name: 'Peach',
+          score: 0
+        },
+        player2: {
+          name: 'Xander',
+          score: 0
+        }
+      }
     },
 
     getPosition: function(boxId) {
@@ -60,7 +97,7 @@ $(document).ready(function() {
       var emptySlots = [];
       for (var row = 0; row < 3; row++) {
         for (var col = 0; col < 3; col++) {
-          if (this.board[row][col] === 0) {
+          if (this.state.board[row][col] === 0) {
             emptySlots.push([row, col]);
           }
         }
@@ -88,16 +125,16 @@ $(document).ready(function() {
       // Check row
       for (var row = 0; row < 3 ; row++) {
         // check empty row
-        if ((this.board[row][0]===0)&&
-            (this.board[row][1]===0)&&
-            (this.board[row][2]===0)) {
+        if ((this.state.board[row][0]===0)&&
+            (this.state.board[row][1]===0)&&
+            (this.state.board[row][2]===0)) {
               potentialPick.push([row,0]);
               potentialPick.push([row,1]);
               potentialPick.push([row,2]);
             }
-        section = [this.board[row][0], this.board[row][1], this.board[row][2]];
+        section = [this.state.board[row][0], this.state.board[row][1], this.state.board[row][2]];
         if (section.sort().toString() === [0,0,2].toString()) {
-          section = [this.board[row][0], this.board[row][1], this.board[row][2]];
+          section = [this.state.board[row][0], this.state.board[row][1], this.state.board[row][2]];
           var index = section.indexOf(2);
           for (var i = 0; i < 3; i++) {
             if (i !== index) {
@@ -108,9 +145,9 @@ $(document).ready(function() {
             }
           }
         }
-        section = [this.board[row][0], this.board[row][1], this.board[row][2]];
+        section = [this.state.board[row][0], this.state.board[row][1], this.state.board[row][2]];
         if (section.sort().toString() === [0,1,1].toString()) {
-          section = [this.board[row][0], this.board[row][1], this.board[row][2]];
+          section = [this.state.board[row][0], this.state.board[row][1], this.state.board[row][2]];
           var index = section.indexOf(0);
           for (var j = 0; j < opponentWin; j++) {
             potentialPick.push([row,index]);
@@ -121,17 +158,17 @@ $(document).ready(function() {
       // Check col
       for (var col = 0; col < 3 ; col++) {
         // check empty col
-        if ((this.board[0][col]===0)&&
-            (this.board[1][col]===0)&&
-            (this.board[2][col]===0)) {
+        if ((this.state.board[0][col]===0)&&
+            (this.state.board[1][col]===0)&&
+            (this.state.board[2][col]===0)) {
               potentialPick.push([0,col]);
               potentialPick.push([1,col]);
               potentialPick.push([2,col]);
             }
         // check my col
-        section = [this.board[0][col], this.board[1][col], this.board[2][col]];
+        section = [this.state.board[0][col], this.state.board[1][col], this.state.board[2][col]];
         if (section.sort().toString() === [0,0,2].toString()) {
-          section = [this.board[0][col], this.board[1][col], this.board[2][col]];
+          section = [this.state.board[0][col], this.state.board[1][col], this.state.board[2][col]];
           var index = section.indexOf(2);
           for (var i = 0; i < 3; i++) {
             if (i !== index) {
@@ -143,7 +180,7 @@ $(document).ready(function() {
           }
         }
         if (section.sort().toString() === [0,1,1].toString()) {
-          section = [this.board[0][col], this.board[1][col], this.board[2][col]];
+          section = [this.state.board[0][col], this.state.board[1][col], this.state.board[2][col]];
           var index = section.indexOf(0);
           for (var j = 0; j < opponentWin; j++) {
             potentialPick.push([index,col]);
@@ -152,19 +189,19 @@ $(document).ready(function() {
         }
       }
       // check diag
-      if ((this.board[0][0]===0)&&(this.board[1][1]===0)&&(this.board[2][2]===0)) {
+      if ((this.state.board[0][0]===0)&&(this.state.board[1][1]===0)&&(this.state.board[2][2]===0)) {
         potentialPick.push([0,0]);
         potentialPick.push([1,1]);
         potentialPick.push([2,2]);
       }
-      if ((this.board[2][0]===0)&&(this.board[1][1]===0)&&(this.board[0][2]===0)) {
+      if ((this.state.board[2][0]===0)&&(this.state.board[1][1]===0)&&(this.state.board[0][2]===0)) {
         potentialPick.push([2,0]);
         potentialPick.push([1,1]);
         potentialPick.push([0,2]);
       }
-      section = [this.board[0][0], this.board[1][1], this.board[2][2]];
+      section = [this.state.board[0][0], this.state.board[1][1], this.state.board[2][2]];
       if (section.sort().toString() === [0,0,2].toString()) {
-        section = [this.board[0][0], this.board[1][1], this.board[2][2]];
+        section = [this.state.board[0][0], this.state.board[1][1], this.state.board[2][2]];
         var index = section.indexOf(2);
         for (var i = 0; i < 3; i++) {
           if (i !== index) {
@@ -176,16 +213,16 @@ $(document).ready(function() {
         }
       }
       if (section.sort().toString() === [0,1,1].toString()) {
-        section = [this.board[0][0], this.board[1][1], this.board[2][2]];
+        section = [this.state.board[0][0], this.state.board[1][1], this.state.board[2][2]];
         var index = section.indexOf(0);
         for (var j = 0; j < opponentWin; j++) {
           potentialPick.push([index,index]);
           console.log('opponent diag 1 ' + index + ' ' + index);
         }
       }
-      section = [this.board[2][0], this.board[1][1], this.board[0][2]];
+      section = [this.state.board[2][0], this.state.board[1][1], this.state.board[0][2]];
       if (section.sort().toString() === [0,0,2].toString()) {
-        section = [this.board[2][0], this.board[1][1], this.board[0][2]];
+        section = [this.state.board[2][0], this.state.board[1][1], this.state.board[0][2]];
         var index = section.indexOf(2);
         for (var i = 0; i < 3; i++) {
           if (i !== index) {
@@ -197,17 +234,14 @@ $(document).ready(function() {
         }
       }
       if (section.sort().toString() === [0,1,1].toString()) {
-        section = [this.board[2][0], this.board[1][1], this.board[0][2]];
+        section = [this.state.board[2][0], this.state.board[1][1], this.state.board[0][2]];
         var index = section.indexOf(0);
         for (var j = 0; j < opponentWin; j++) {
           potentialPick.push([2-index,index]);
           console.log('opponent diag 2 ' + 2-index + ' ' + index);
         }
       }
-      // console.log(potentialPick.sort());
       var randomIndex = Math.floor(Math.random()*potentialPick.length);
-      console.log(potentialPick.sort());
-      console.log(randomIndex);
       return potentialPick[randomIndex];
 
     },
@@ -221,76 +255,74 @@ $(document).ready(function() {
     },
 
     pushInput: function(row, col) {
-      if(this.currentPlayer === 1 && this.winner === 0) {
-        if (this.board[row][col] === 0) {
-          this.board[row][col] = 1;
+      console.log('turnCounter ' + this.state.turnCounter);
+      if(this.state.currentPlayer === 1 && this.state.winner === 0 && this.state.board[row][col] === 0) {
+        this.state.board[row][col] = 1;
+        $('#' + row + '-' + col).removeClass("clear");
+        $('#' + row + '-' + col).addClass("o");
+        this.state.currentPlayer = 2;
+        this.state.turnCounter++;
+        this.checkWinner(row, col);
+        if (this.isSinglePlay && (this.state.turnCounter < 9)) {
+          var resArray = this.AIturn();
+          var row = resArray[0];
+          var col = resArray[1];
+          this.state.board[row][col] = 2;
           $('#' + row + '-' + col).removeClass("clear");
-          $('#' + row + '-' + col).addClass("o");
-          this.currentPlayer = 2;
-          this.turnCounter++;
+          $('#' + row + '-' + col).addClass("x");
+          this.state.currentPlayer = 1;
+          this.state.turnCounter++;
           this.checkWinner(row, col);
-          console.log(this.turnCounter);
-          if (this.isSinglePlay && (this.turnCounter < 9)) {
-            var resArray = this.AIturn();
-            var row = resArray[0];
-            var col = resArray[1];
-            this.board[row][col] = 2;
-            $('#' + row + '-' + col).removeClass("clear");
-            $('#' + row + '-' + col).addClass("x");
-            this.currentPlayer = 1;
-            this.turnCounter++;
-            this.checkWinner(row, col);
-          }
         }
       } else {
-        if (this.board[row][col] === 0 && this.winner === 0) {
-          if (this.isSinglePlay) {
-            var resArray = this.AIturn();
-            var row = resArray[0];
-            var col = resArray[1];
-            this.board[row][col] = 2;
+        if (this.state.board[row][col] === 0 && this.state.winner === 0) {
+          // if (this.isSinglePlay) {
+          //   var resArray = this.AIturn();
+          //   var row = resArray[0];
+          //   var col = resArray[1];
+          //   this.state.board[row][col] = 2;
+          //   $('#' + row + '-' + col).removeClass("clear");
+          //   $('#' + row + '-' + col).addClass("x");
+          //   this.state.currentPlayer = 1;
+          //   this.state.turnCounter++;
+          //   this.checkWinner(row, col);
+          // } else {
+            this.state.board[row][col] = 2;
             $('#' + row + '-' + col).removeClass("clear");
             $('#' + row + '-' + col).addClass("x");
-            this.currentPlayer = 1;
-            this.turnCounter++;
+            this.state.currentPlayer = 1;
+            this.state.turnCounter++;
             this.checkWinner(row, col);
-          } else {
-            this.board[row][col] = 2;
-            $('#' + row + '-' + col).removeClass("clear");
-            $('#' + row + '-' + col).addClass("x");
-            this.currentPlayer = 1;
-            this.turnCounter++;
-            this.checkWinner(row, col);
-          }
+          // }
         }
       }
-
-      // console.log("board: " + this.board);
+      localStorage.setItem("tictactoeState", JSON.stringify(tictactoe.state));
+      // console.log("state.board: " + this.state.board);
       // console.log("winner: " + this.winner);
     },
 
     checkWinner: function(row, col) {
       // Checking process
-      if ((this.board[row][0] === this.board[row][1]) && (this.board[row][1] === this.board[row][2])) {
-        this.winner = this.board[row][0];
-      } else if ((this.board[0][col] === this.board[1][col]) && (this.board[1][col] === this.board[2][col])) {
-          this.winner = this.board[0][col];
+      if ((this.state.board[row][0] === this.state.board[row][1]) && (this.state.board[row][1] === this.state.board[row][2])) {
+        this.state.winner = this.state.board[row][0];
+      } else if ((this.state.board[0][col] === this.state.board[1][col]) && (this.state.board[1][col] === this.state.board[2][col])) {
+          this.state.winner = this.state.board[0][col];
       } else if((row+col)%2 === 0) {
-        if (((this.board[0][0] === this.board[1][1]) && (this.board[1][1] === this.board[2][2])) ||
-            ((this.board[2][0] === this.board[1][1]) && (this.board[1][1] === this.board[0][2]))) {
-          this.winner = this.board[1][1];
+        if (((this.state.board[0][0] === this.state.board[1][1]) && (this.state.board[1][1] === this.state.board[2][2])) ||
+            ((this.state.board[2][0] === this.state.board[1][1]) && (this.state.board[1][1] === this.state.board[0][2]))) {
+          this.state.winner = this.state.board[1][1];
         }
       }
       // Winner message
-      if (this.winner !== 0 || this.turnCounter === 9) {
-        if (this.winner !== 0) {
-          scoreBoard['player'+this.winner]++;
+      if (this.state.winner !== 0 || this.state.turnCounter === 9) {
+        if (this.state.winner !== 0) {
+          this.state.scoreBoard['player'+this.state.winner].score++;
           updateScoreboard();
-          $winnerMessage = $('#winner-message');
-          $winnerMessage.html('The winner is player ' + this.winner + '!').show();
+          $winnerMessage.html('The winner is player ' + this.state.winner + '!').show();
         } else {
           $winnerMessage = $('#winner-message');
           $winnerMessage.html('Draw!').show();
+          updateScoreboard();
         }
         $('.ui.modal.small').modal('show');
 
@@ -299,7 +331,6 @@ $(document).ready(function() {
   };
 
   initializeGame();
-  updateScoreboard();
   $('.ui.modal.small').modal('hide');
 
   $(document).on('click', '.box', function() {
@@ -308,25 +339,18 @@ $(document).ready(function() {
   });
 
   $(document).on('click', '#new-game', function() {
-    $("#board").fadeOut(800, function() {
-      initializeGame();
-    });
-    $("#board").fadeIn(800);
+    resetGame();
   });
 
   $(document).on('click', '#new-game-popUp', function() {
     $('.ui.modal.small').modal('hide');
-    $("#board").fadeOut(800, function() {
-      initializeGame();
-    });
-    $("#board").fadeIn(800);
+    resetGame();
   });
 
   $(document).on('click', '#clear-board', function() {
-    scoreBoard.player1 = 0;
-    scoreBoard.player2 = 0;
-    initializeGame();
-    updateScoreboard();
+    tictactoe.state.scoreBoard.player1.score = 0;
+    tictactoe.state.scoreBoard.player2.score = 0;
+    resetGame();
   });
 
   $(document).on('click', '#board-size', function() {
@@ -344,10 +368,15 @@ $(document).ready(function() {
   });
 
   $(document).on('click', '#single-play', function() {
-    scoreBoard.player1 = 0;
-    scoreBoard.player2 = 0;
-    initializeGame();
-    updateScoreboard();
-    $('#single-play').addClass('primary');
+    if (tictactoe.isSinglePlay === true) {
+      $('#single-play').removeClass('primary').html('Play with me');
+      tictactoe.isSinglePlay = false;
+    } else {
+      $('#single-play').addClass('primary').html('Play with AI');
+      tictactoe.isSinglePlay = true;
+    }
+    tictactoe.state.scoreBoard.player1.score = 0;
+    tictactoe.state.scoreBoard.player2.score = 0;
+    resetGame();
   })
 })
